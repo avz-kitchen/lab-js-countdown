95992cafa21c3d8cc04f96ed06db20d9c872913c
AValenzue <zuellagraphy@gmail.com>
2024-04-18 11:30:00 +0200

    Solved labs and changed timer display to cats

--- src/index.js
+++ src/index.js
@@ -7,16 +7,45 @@ let timer = null; // Variable to store the interval
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
-
-
-
+const startButton = document.getElementById("start-btn");
+startButton.addEventListener("click", function() {
+  startCountdown(10);
+});
 
 // ITERATION 2: Start Countdown
-function startCountdown() {
-  console.log("startCountdown called!");
-
+function startCountdown(timer) {
+  console.log("startCountdown called!")
 
   // Your code goes here ...
+  startButton.disabled = true;
+
+  let remainingTime = timer; 
+  console.log(`Remaining time: ${remainingTime} seconds`);
+  updateTimeDisplay(remainingTime);
+  const interval = setInterval(() => {
+    remainingTime--; 
+    console.log(`Remaining time: ${remainingTime} seconds`);
+    updateTimeDisplay(remainingTime);
+    if (remainingTime === 0) {
+      clearInterval(interval);
+      console.log("Countdown finished!");
+      showToast();
+      startButton.disabled = false;
+
+
+    }
+},1000)
+
+}
+function updateTimeDisplay(time) {
+  const timeElement = document.getElementById('time');
+  timeElement.textContent = `${time}`;
+  let emoji = '';
+  for (let i = 0; i < time; i++) {
+    emoji += '⏳'; // You can replace '⏳' with any emoji you like
+  }
+
+  timeElement.textContent = emoji;
 }
 
 
@@ -28,6 +57,11 @@ function showToast(message) {
 
   // Your code goes here ...
 
+  const toastElement = document.getElementById('toast');
+  toastElement.classList.add('show');
+  setTimeout(() => {
+    toastElement.classList.remove('show');
+  }, 3000); 
 
 
 
